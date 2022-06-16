00869347d6a87f13285c27c04f37ed27a4b9d32f
contoldoonanos <107590062+contoldoonanos@users.noreply.github.com>
2022-06-16 17:37:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const rs = require('readline-sync');
 
 const GoStumble = (auth) => new Promise((resolve, reject) => {
 
-    fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+    fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/2', {
         method: 'GET',
         headers: {
             'authorization': auth
